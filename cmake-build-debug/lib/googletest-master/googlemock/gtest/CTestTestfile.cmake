81a12e4606ee5b9f4d7ab208f26f36fdc9792978
alexandrebignalet <alexandre.bignalet@viacesi.fr>
2017-02-02 15:24:55 -0500

    remove cmake-build-debug from git

--- cmake-build-debug/lib/googletest-master/googlemock/gtest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Alexandre/Documents/UQAM/principes_avances_des_langages_a_objets/FarStar/lib/googletest-master/googletest
-# Build directory: C:/Users/Alexandre/Documents/UQAM/principes_avances_des_langages_a_objets/FarStar/cmake-build-debug/lib/googletest-master/googlemock/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
